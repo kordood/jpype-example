ea57240e31d01488ff983fa84d8c8280c1e17f88
kordood <gigacms@gmail.com>
2021-08-23 12:54:47 +0900

    Rename variables straightforwardly

--- python/src/testjpype_1.py
+++ python/src/testjpype_1.py
@@ -10,17 +10,17 @@ def TestMain():
 
     jpype.startJVM(jpype.getDefaultJVMPath(), "-Djava.class.path=%s" % classpath)
 
-    testPkg = jpype.JPackage('testjpype') # get the package
+    package_py = jpype.JPackage('testjpype') # get the package
 
-    TestJPype = testPkg.TestJPype # get the class
+    class_py = package_py.TestJPype # get the class
 
-    jc = TestJPype() # create an instance of the class
+    instance_py = class_py() # create an instance of the class
 
-    jc.speak("This is a test message") # try to call one of the class methods
+    instance_py.speak("This is a test message") # try to call one of the class methods
 
-    jc.setString("Hello, World") # set a string
+    instance_py.setString("Hello, World") # set a string
 
-    s = jc.getString() # get the string back
+    s = instance_py.getString() # get the string back
 
     print(s)
 
