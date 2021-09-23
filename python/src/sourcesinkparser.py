f6d6936f264cad40490fd912663e7f55656b128a
kordood <gigacms@gmail.com>
2021-09-23 16:26:28 +0900

    Feat: Parse Source Sink using parser

--- python/src/sourcesinkparser.py
+++ python/src/sourcesinkparser.py
@@ -1,16 +1,23 @@
-test_str = [
-    "<com.novell.ldap.rfc2251.RfcFilter: void addSubstring(int,byte[])> -> _SINK_",
-    "<org.xmldb.api.base.XMLDBException: void printStackTrace(java.io.PrintWriter)> -> _SINK_",
-    "<org.springframework.security.config.http.FormLoginBeanDefinitionParser: java.lang.String getLoginPage)> -> _SOURCE_",
+class SourceSink:
 
+    def __init__(self, type, class_name, method_name, ret_type, params):
+        self.type = type
+        self.class_name = class_name
+        self.method_name = method_name
+        self.ret_type = ret_type
+        self.params = params
 
-]
+    def __str__(self):
+        return "[%s] %s %s.%s%s" % (self.type.upper(), self.ret_type, self.class_name, self.method_name, self.params)
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class SourceSinkParser:
 
-    signature = {'source': "_SOURCE_",
-                 'sink': "_SINK_"
+    signature = {'_SOURCE_': 'source',
+                 '_SINK_': 'sink'
                  }
 
     def __init__(self, sourcesinks):
@@ -19,3 +26,61 @@ class SourceSinkParser:
         self.sources = [ ]
         self.sinks = [ ]
         self.parse()
+
+    def parse(self):
+        for line in self.sourcesinks:
+            line = line.replace('\n', '')
+
+            if any(key in line for key in self.signature.keys()):
+                define, type, permission = self.parse_line(line)
+                class_name, method = self.parse_define(define)
+                ret_type, method_name, params = self.parse_method(method)
+                source_sink = SourceSink(type, class_name, method_name, ret_type, params)
+                if source_sink.type == 'source':
+                    self.sources.append(source_sink)
+                elif source_sink.type == 'sink':
+                    self.sinks.append(source_sink)
+
+    def parse_line(self, line: str):
+        split_str = line.split(' -> ')
+        assert len(split_str) == 2, "[E] parse type error %s" % line
+
+        define = split_str[0]
+        type = self.signature[split_str[1].strip()]
+        permission = ""
+
+        if '> ' in split_str[0]:
+            temp_split_str = split_str[0].split('> ')
+            permission = temp_split_str[1].strip()
+            define = temp_split_str[0] + '>'
+
+        return define, type, permission
+
+    @staticmethod
+    def parse_define(define: str):
+        define = define.lstrip('<')
+        define = define.rstrip('>')
+        split_str = [split.strip() for split in define.split(':')]
+        assert len(split_str) == 2, "[E] parse define error %s" % define
+
+        return split_str[0], split_str[1]
+
+    @staticmethod
+    def parse_method(method):
+        split_str = method.split(' ', 1)
+        assert len(split_str) == 2, "[E] parse method error %s" % method
+
+        ret_type = split_str[0]
+
+        if '(' in split_str[1]:
+            split_str = split_str[1].split('(')
+            method_name = split_str[0]
+            assert len(split_str) == 2, "[E] parse params error %s" % split_str[1]
+
+            params = tuple(param for param in split_str[1].replace(' ', '').rstrip(')').split(','))
+        else:
+            split_str = split_str[1].split(')')
+            method_name = split_str[0]
+            params = tuple()
+
+        return ret_type, method_name, params
